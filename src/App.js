4296dfae18ef3f8b6d3b50e7a7dce3ff0e54e6b1
hanurakesh16 <hanurakesh11@gmail.com>
2023-03-10 14:35:15 +0530

    states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      name: {
+        firstName: 'John',
+        lastName: 'David'
+      },
+      company: 'DC Studios'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome, {this.state.name.firstName} {this.state.name.lastName} from {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState({name: { firstName: 'Bruce', lastName: 'Wayne' }});
+          }}>Change</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
