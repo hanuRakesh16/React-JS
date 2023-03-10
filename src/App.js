3e6698edee75adf51c4dbdeeb38848c74d207d96
hanurakesh16 <hanurakesh11@gmail.com>
2023-03-10 16:04:39 +0530

    api and component mount

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
 import { Component } from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor () {
     super();
     this.state = {
-      name: {
-        firstName: 'John',
-        lastName: 'David'
-      },
-      company: 'DC Studios'
+     monsters: [],
     }
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(() => {
+          return { monsters: users }
+        }, () => {
+          console.log(this.state)
+        })
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome, {this.state.name.firstName} {this.state.name.lastName} from {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState(
-              () => {
-              return {
-                name: { firstName: 'Bruce', lastName: 'Wayne' }
-              }
-            }, 
-              () => {
-                console.log(this.state);   //callback function
-            });
-          }}>Change</button>
-        </header>
+        {
+          this.state.monsters.map( (monster) => {
+            return <div key={monster.id}><h1>{monster.name}</h1></div>
+          })
+        }
       </div>
     );
   }
